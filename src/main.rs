8e6ee6ae7904370833c0f57ef695c93863d0eb21
Šimon Brandner <simon.bra.ag@gmail.com>
2024-06-20 22:13:42 +0200

    Import FaceONNX

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+mod model;
+
+//use burn::tensor;
+//use burn_ndarray::{NdArray, NdArrayDevice};
+//use model::face::Model;
+
 fn main() {
-    println!("Hello world!");
+	println!("Hello, world!")
+	// Initialize a new model instance
+	//let device = NdArrayDevice::default();
+	//let model: Model<NdArray<f32>> = Model::new(&device);
+	//
+	//// Create a sample input tensor (zeros for demonstration)
+	//let input = tensor::Tensor::<NdArray<f32>, 4>::zeros([1, 1, 28, 28], &device);
+	//
+	//// Perform inference
+	//let output = model.forward(input);
+
+	// Print the output
+	//println!("{:?}", output);
 }
---
+++ src/model/mod.rs
@@ -0,0 +1,3 @@
+pub mod face {
+	include!(concat!(env!("OUT_DIR"), "/model/face.rs"));
+}
