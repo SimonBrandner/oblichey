53394c9bd83d5446f200bf0147187facb141d1e1
Šimon Brandner <simon.bra.ag@gmail.com>
2024-07-21 14:54:24 +0200

    Make a bunch of things public
    Signed-off-by: Šimon Brandner <simon.bra.ag@gmail.com>

--- src/processor.rs
+++ src/processor.rs
@@ -2,42 +2,41 @@
 //use model::detector::Model;
 //use model::recognizer::Model;
 //use burn_ndarray::{ NdArrayDevice, NdArray };
-//use v4l::io::traits::Stream;
 use image::RgbaImage;
 
 #[derive(Debug, Clone)]
-struct FaceData;
+pub struct FaceData;
 
 #[derive(Debug, Clone)]
-struct Coordinates {
-	x: usize,
-	y: usize,
+pub struct Coordinates {
+	pub x: usize,
+	pub y: usize,
 }
 
 #[derive(Debug, Clone)]
-struct FaceCoordinates {
-	location: Coordinates,
-	size: Coordinates,
+pub struct FaceCoordinates {
+	pub location: Coordinates,
+	pub size: Coordinates,
 }
 
 #[derive(Debug, Clone, Default)]
-pub struct ScanAuthenticatorState {
-	face_coordinates: Option<FaceCoordinates>,
-	date: Option<FaceData>,
+pub struct ScanProcessorState {
+	pub face_coordinates: Option<FaceCoordinates>,
+	pub date: Option<FaceData>,
 }
 #[derive(Debug, Clone, Default)]
-pub struct AuthAuthenticatorState {
-	face_coordinates: Option<FaceCoordinates>,
-	authenticated: bool,
+pub struct AuthProcessorState {
+	pub face_coordinates: Vec<FaceCoordinates>,
+	pub authenticated: bool,
 }
 
 #[derive(Clone)]
-pub enum AuthenticatorState {
-	Scan(ScanAuthenticatorState),
-	Auth(AuthAuthenticatorState),
+pub enum ProcessorState {
+	Scan(ScanProcessorState),
+	Auth(AuthProcessorState),
 }
 pub struct Processor {
-	state: AuthenticatorState,
+	state: ProcessorState,
 }
 
 impl Processor {
@@ -46,11 +45,11 @@ impl Processor {
 		//let model: Model<NdArray<f32>> = Model::new(&device);
 
 		Self {
-			state: AuthenticatorState::Scan(ScanAuthenticatorState::default()),
+			state: ProcessorState::Scan(ScanProcessorState::default()),
 		}
 	}
 
-	pub fn process_frame(&self, _frame_buffer: &RgbaImage) -> AuthenticatorState {
+	pub fn process_frame(&self, _frame_buffer: &RgbaImage) -> ProcessorState {
 		//let input = tensor::Tensor::<NdArray<f32>, 4>::zeros([1, 1, 28, 28], &device);
 		//let output = model.forward(input);
 
