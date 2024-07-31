05b5116c653e65f8d24fa6a1b68c785068ea2793
Šimon Brandner <simon.bra.ag@gmail.com>
2024-07-31 18:35:04 +0200

    Try to fix `build.rs`
    Signed-off-by: Šimon Brandner <simon.bra.ag@gmail.com>
    
    bla
    
    Signed-off-by: Šimon Brandner <simon.bra.ag@gmail.com>

--- src/build.rs
+++ src/build.rs
@@ -2,12 +2,12 @@ use burn_import::onnx::ModelGen;
 
 fn main() {
 	ModelGen::new()
-		.input("models/detector.onnx")
-		.out_dir("models/")
+		.input("./models/detector.onnx")
+		.out_dir("./src/models/")
 		.run_from_script();
 
 	ModelGen::new()
-		.input("models/recognizer.onnx")
-		.out_dir("models/")
+		.input("./models/recognizer.onnx")
+		.out_dir("./src/models/")
 		.run_from_script();
 }
--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,7 +1,7 @@
 pub mod detector {
-	include!(concat!(env!("OUT_DIR"), "/models/detector.rs"));
+	include!(concat!(env!("OUT_DIR"), "/src/models/detector.rs"));
 }
 
 pub mod recognizer {
-	include!(concat!(env!("OUT_DIR"), "/models/recognizer.rs"));
+	include!(concat!(env!("OUT_DIR"), "/src/models/recognizer.rs"));
 }
