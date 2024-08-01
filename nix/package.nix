d6e8bbfd13c9f96145cb4c18ac641e18a4622e06
Šimon Brandner <simon.bra.ag@gmail.com>
2024-08-01 19:57:23 +0200

    Make `nix build` work
    Fixes https://github.com/SimonBrandner/gday/issues/12
    
    Signed-off-by: Šimon Brandner <simon.bra.ag@gmail.com>

--- nix/package.nix
+++ nix/package.nix
@@ -2,15 +2,50 @@
   lib,
   rustPlatform,
   clang,
+  pkgs ? import <nixpkgs> {},
 }: let
   cargoToml = builtins.fromTOML (builtins.readFile ../Cargo.toml);
 in
-  rustPlatform.buildRustPackage {
+  rustPlatform.buildRustPackage rec {
     inherit (cargoToml.package) version;
     pname = cargoToml.package.name;
-    cargoLock.lockFile = ../Cargo.lock;
+    cargoLock = {
+      lockFile = ../Cargo.lock;
+      outputHashes = {
+        "burn-0.14.0" = "sha256-5ssNI+QvMpEeU7JpK1KVtBpTgXKbaqTUYTBePTL+J24=";
+        "cubecl-0.1.1" = "sha256-xgwXyHAKSxf4rTF/Svpb61IOvxQUiV9xmOXk5WHDdjY=";
+      };
+    };
     src = lib.cleanSource ../.;
-    buildInputs = [
+    nativeBuildInputs = with pkgs; [
       clang
+      fontconfig
+      pkg-config
+      libxkbcommon
+      libGL
+      cmake
+
+      wayland
+
+      xorg.libXcursor
+      xorg.libXrandr
+      xorg.libXi
+      xorg.libX11
+
+      vulkan-headers
+      vulkan-loader
+
+      rustfmt
     ];
+    buildInputs = with pkgs; [
+      fontconfig
+      vulkan-headers
+      vulkan-loader
+    ];
+    preInstall = ''
+      mkdir -p $out/bin
+      cp -r target/release/weights $out/bin/weights
+    '';
+    LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
+    LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath buildInputs}";
   }
