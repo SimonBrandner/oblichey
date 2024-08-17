407a8a3764942b6cb7c1e8b7a07fa8e3b040947b
Šimon Brandner <simon.bra.ag@gmail.com>
2024-08-17 17:41:30 +0200

    Rewrap text
    Signed-off-by: Šimon Brandner <simon.bra.ag@gmail.com>

--- crates/gday-pam-module/src/lib.rs
+++ crates/gday-pam-module/src/lib.rs
@@ -14,9 +14,9 @@ impl PamHooks for GdayPamModule {
 	fn sm_authenticate(_pamh: &mut PamHandle, _args: Vec<&CStr>, _flags: PamFlag) -> PamResultCode {
 		println!("Starting face recognition");
 
-		// This is one of the ugliest things I have done recently and there really ought to be
-		// a way to do this other than calling another executable. Ideally, we would make the
-		// core code into some kind of a shared library.
+		// This is one of the ugliest things I have done recently and there really ought to be a
+		// way to do this other than calling another executable. Ideally, we would make the core
+		// code into some kind of a shared library.
 		match Command::new(EXECUTABLE_PATH).arg("auth").output() {
 			Ok(o) => {
 				if o.status.success() {
