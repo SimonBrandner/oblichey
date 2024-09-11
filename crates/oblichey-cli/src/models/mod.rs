6c2fe1b35107dfb59201475a893ddd15549d19f6
Šimon Brandner <simon.bra.ag@gmail.com>
2024-09-11 11:22:43 +0200

    Use a different weights path in tests
    Signed-off-by: Šimon Brandner <simon.bra.ag@gmail.com>

--- crates/oblichey-cli/src/models/mod.rs
+++ crates/oblichey-cli/src/models/mod.rs
@@ -6,13 +6,18 @@ pub mod recognizer;
 const WEIGHTS_DIRECTORY_NAME: &str = "weights";
 
 fn get_weights_file(model_name: &str) -> String {
-	let path_to_executable = std::env::current_exe().expect("Could not get path to executable!");
-	let path_to_dir_with_executable = path_to_executable
-		.parent()
-		.expect("Could not get parent directory!");
+	const PARENT_DIR_ERROR: &str = "Could not get parent directory!";
 
-	path_to_dir_with_executable
-		.join(WEIGHTS_DIRECTORY_NAME)
+	let mut path = std::env::current_exe().expect("Could not get path to executable!");
+	path = path.parent().expect(PARENT_DIR_ERROR).to_path_buf();
+
+	// In tests we need to go further up for some reason
+	#[cfg(test)]
+	{
+		path = path.parent().expect(PARENT_DIR_ERROR).to_path_buf();
+	}
+
+	path.join(WEIGHTS_DIRECTORY_NAME)
 		.join(String::new() + model_name + ".mpk")
 		.to_str()
 		.expect("Failed to get valid UTF-8 string from path!")
